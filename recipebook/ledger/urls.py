1fa5fc1d14a83d3e0b02db5f71979be92fa7de2b
Juan <dad3ztroyah@gmail.com>
2025-02-24 15:32:05 +0800

    Added and implemented templates folder and templates

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -3,8 +3,8 @@ from .views import ledger, recipe1, recipe2
 
 urlpatterns = [
     path('recipes/list/', ledger, name = "ledger"),
-    path('recipe/1', recipe1, name = 'recipe1'),
-    path('recipe/2', recipe2, name = 'recipe2'),
+    path('recipe/1/', recipe1, name = 'recipe1'),
+    path('recipe/2/', recipe2, name = 'recipe2'),
 ]
 
 app_name = "ledger"
