11b0793ae55e2c107b5537949db0ea5f466c62b4
Juan <dad3ztroyah@gmail.com>
2025-03-07 16:43:25 +0800

    Merged recipe paths together, now utilizes pk as a parameter. Adjusted ledger and recipe paths to utilize new views.py classes.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import ledger, recipe1, recipe2
+from .views import ledger, recipe
 
 urlpatterns = [
-    path('recipes/list/', ledger, name = "ledger"),
-    path('recipe/1/', recipe1, name = 'recipe1'),
-    path('recipe/2/', recipe2, name = 'recipe2'),
+    path('recipes/list/', ledger.as_view(), name = "ledger"),
+    path('recipe/int:<pk>/', recipe.as_view(), name = 'recipe'),
 ]
 
 app_name = "ledger"
