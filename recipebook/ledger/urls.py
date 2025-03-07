d231f5db2cb0d8bcf734b88daf87a6aa2322969f
Juan <dad3ztroyah@gmail.com>
2025-03-07 16:52:45 +0800

    Fixed syntax for recipe path

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -3,7 +3,7 @@ from .views import ledger, recipe
 
 urlpatterns = [
     path('recipes/list/', ledger.as_view(), name = "ledger"),
-    path('recipe/int:<pk>/', recipe.as_view(), name = 'recipe'),
+    path('recipe/<int:pk>/', recipe.as_view(), name = 'recipe'),
 ]
 
 app_name = "ledger"
