73bbfdcb861c8ebbcdc8d28523869d8e5c8c0b16
Juan <dad3ztroyah@gmail.com>
2025-03-07 19:50:51 +0800

    Added admin customization for each model. Added displays and filters for each model. Added inline editing for Recipe and Ingredient

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -2,14 +2,26 @@ from django.contrib import admin
 from .models import Recipe, Ingredient, RecipeIngredient
 
 # Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
 class RecipeAdmin(admin.ModelAdmin):
-    model = Recipe
+    inlines = [RecipeIngredientInline,]
+    search_fields = ('name',)
+    list_display = ('name',)
+    list_filter = ('name',)
 
 class IngredientAdmin(admin.ModelAdmin):
-    model = Ingredient
+    inlines = [RecipeIngredientInline,]
+    search_fields = ('name',)
+    list_display = ('name',)
+    list_filter = ('name',)
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
+    search_fields = ('recipe', 'ingredient', 'quantity',)
+    list_display = ('recipe', 'ingredient', 'quantity',)
+    list_filter = ('recipe', 'ingredient', 'quantity',)
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
