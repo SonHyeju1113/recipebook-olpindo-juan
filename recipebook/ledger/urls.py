161bd406af2ca4c97c646f31de8c76da6836a824
Juan <dad3ztroyah@gmail.com>
2025-03-22 18:13:08 +0800

    Added login view to urls.py. Fixed login url and redirect in settings.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import ledger, recipe
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('recipes/list/', ledger.as_view(), name = "ledger"),
-    path('recipe/<int:pk>/', recipe.as_view(), name = 'recipe'),
+    path('recipe/<int:pk>/', recipe.as_view(), name = "recipe"),
+    path('accounts/login/', LoginView.as_view(), name='login')
 ]
 
 app_name = "ledger"
