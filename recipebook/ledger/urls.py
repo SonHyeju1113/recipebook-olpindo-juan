5a7a108b61d927e2bf769847f86f0efb6c185644
Juan <dad3ztroyah@gmail.com>
2025-04-07 21:55:54 +0800

    Fixed naming conventions for views

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ledger, recipe
+from .views import Ledger, Recipe
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('recipes/list/', ledger.as_view(), name = "ledger"),
-    path('recipe/<int:pk>/', recipe.as_view(), name = "recipe"),
+    path('recipes/list/', Ledger.as_view(), name = "ledger"),
+    path('recipe/<int:pk>/', Recipe.as_view(), name = "recipe"),
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('', LoginView.as_view(), name='home')
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -3,14 +3,14 @@ from django.views.generic.detail import DetailView
 from .models import Recipe
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ledger(LoginRequiredMixin, ListView):
+class Ledger(LoginRequiredMixin, ListView):
     model = Recipe
     context_object_name = 'ledger'
-    template_name = 'ledger.html'
+    template_name = 'ledger/ledger.html'
     redirect_field_name = 'accounts/login'
 
-class recipe(LoginRequiredMixin, DetailView):
+class Recipe(LoginRequiredMixin, DetailView):
     model = Recipe
     context_object_name = 'recipe'
-    template_name = 'recipe.html'
+    template_name = 'ledger/recipe.html'
     redirect_field_name = 'accounts/login'
