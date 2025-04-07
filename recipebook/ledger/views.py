114443f71eb788eb71b2eb3e4a2ce000f44c8e00
Juan <dad3ztroyah@gmail.com>
2025-04-08 01:34:20 +0800

    Added CreateView for each model except for profile

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,34 +1,68 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Recipe
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Recipe, Ingredient, RecipeIngredient
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import RecipeForm, RecipeIngredientForm, RecipeImageForm
+from .forms import RecipeForm, IngredientForm, RecipeIngredientForm, RecipeImageForm
+from django.urls import reverse_lazy
 
-class Ledger(LoginRequiredMixin, ListView):
+class LedgerView(LoginRequiredMixin, ListView):
     model = Recipe
     context_object_name = 'ledger'
     template_name = 'ledger/ledger.html'
     redirect_field_name = 'accounts/login'
 
-class Recipe(LoginRequiredMixin, DetailView):
+class RecipeView(LoginRequiredMixin, DetailView):
     model = Recipe
     context_object_name = 'recipe'
     template_name = 'ledger/recipe.html'
     redirect_field_name = 'accounts/login'
+        
+class RecipeCreate(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'ledger/recipe_add.html'
+
+class RecipeUpdate(LoginRequiredMixin, UpdateView):
+    model = Recipe
+    template_name = 'ledger/recipe.html'
+    form_class = RecipeForm
+
+class IngredientCreate(LoginRequiredMixin, CreateView):
+    model = Ingredient
+    form_class = IngredientForm
+    template_name = 'ledger/ingredient_add.html'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:create')
+    
+class RecipeIngredientCreate(LoginRequiredMixin, CreateView):
+    model = RecipeIngredient
+    form_class = RecipeIngredientForm
+    template_name = 'ledger/recipeingredient_add.html'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:create')
+
+class RecipeImageCreate(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeImageForm
+    template_name = 'ledger/image_add.html'
 
     def get_context_data(self, **kwargs):
-        ctx = super(Recipe, self).get_context_data(**kwargs)
-        ctx['form'] = RecipeForm()
+        ctx = super().get_context_data(**kwargs)
+        ctx['pk'] = self.kwargs['pk']
         return ctx
+    
+    def form_valid(self, form):
+        form.instance.recipe = Recipe.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
 
-    def post(self, request, *args, **kwargs):
-        form = RecipeForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return self.get(request, *args, **kwargs)
-        else:
-            self.object_list = self.get_queryset(**kwargs)
-            context = self.get_context_data(**kwargs)
-            context['form'] = form
-            return self.render_to_response(context)
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe', kwargs={'pk': self.kwargs['pk']})
