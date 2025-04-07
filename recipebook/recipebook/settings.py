ceb437cfa545f6fd7d96d087abdfa665d24fa28e
Juan <dad3ztroyah@gmail.com>
2025-04-07 19:08:55 +0800

    Added media url in settings.py

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -129,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/recipes/list/'
 LOGOUT_REDIRECT_URL = 'login'
+
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
