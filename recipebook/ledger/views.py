482e1435b38f01668006d8450f7d2aba23db8cb6
Juan <dad3ztroyah@gmail.com>
2025-04-07 22:39:50 +0800

    Added form functions for Recipe

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,6 +2,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Recipe
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import RecipeForm, RecipeIngredientForm, RecipeImageForm
 
 class Ledger(LoginRequiredMixin, ListView):
     model = Recipe
@@ -14,3 +15,20 @@ class Recipe(LoginRequiredMixin, DetailView):
     context_object_name = 'recipe'
     template_name = 'ledger/recipe.html'
     redirect_field_name = 'accounts/login'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(Recipe, self).get_context_data(**kwargs)
+        ctx['form'] = RecipeForm()
+        return ctx
+
+    def post(self, request, *args, **kwargs):
+        form = RecipeForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return self.get(request, *args, **kwargs)
+        else:
+            self.object_list = self.get_queryset(**kwargs)
+            context = self.get_context_data(**kwargs)
+            context['form'] = form
+            return self.render_to_response(context)
